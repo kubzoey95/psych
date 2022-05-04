3fa8d00655a4fe962bf9c8b713ed8ea39196793c
Jakub Belicki <jakubsliwice@o2.pl>
2022-05-05 00:02:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
 const fs = new LightningFS('fs')
 
 const dir = '/test-clone'
-git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs', corsProxy: 'https://cors.isomorphic-git.org' }).then(console.log)
+git.clone({ fs, http, dir, url: 'https://github.com/kubzoey95/psych'}).then(console.log)
