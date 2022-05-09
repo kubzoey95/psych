1a8fb5ce9942c4c25b1ea5f577036eb7731af0d1
Jakub Belicki <jakubsliwice@o2.pl>
2022-05-09 09:47:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Octokit, App } from "https://cdn.skypack.dev/octokit";
+
 let key = CryptoJS.SHA3(prompt("Password please"), { outputLength: 512 });
 
 console.log(key.toString())
@@ -10,5 +12,17 @@ await jQuery.get('https://raw.githubusercontent.com/kubzoey95/psych/master/token
 });
 
 let decrypted = CryptoJS.AES.decrypt(token, key.toString());
+
+const octokit = new Octokit({
+  auth: decrypted.toString(CryptoJS.enc.Utf8)
+})
+
+let ref = await octokit.request('GET /repos/{owner}/{repo}/git/ref/{ref}', {
+  owner: 'kubzoey95',
+  repo: 'psych',
+  ref: 'heads/master'
+})
+
+console.log(ref)
 
 console.log(decrypted.toString(CryptoJS.enc.Utf8))
